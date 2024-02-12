274e1f535cf72d90212954cea5ee4d985e0615fc
Tyagidev8989 <157591313+Tyagidev8989@users.noreply.github.com>
2024-02-13 00:22:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
   
     start_text = "**Hello {}!\nI'm an auto approve Admin Join Requests Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n💎Powerd By : @Tyagidev999**"               
     
-    start_text1 = "**Why Should You Play On Winbuzz?
+    cap="**Why Should You Play On Winbuzz?
 www.winbuzz.in
 www.winbuzz.in
 
