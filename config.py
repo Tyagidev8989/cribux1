c69e699bd5ffe1baa06c15bd7e2f6c16e657f5cd
Tyagidev8989 <157591313+Tyagidev8989@users.noreply.github.com>
2024-02-13 07:46:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
   
     start_text = "**Hello {}!\nI'm an auto approve Admin Join Requests Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n💎Powerd By : @Tyagidev999**"               
     
-    cap="**HY**"
+    cap="**HY\n hy\n hy**"
 
         
   
